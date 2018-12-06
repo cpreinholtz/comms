de29fe00122f1d5dfb2b984ba49d3e464f8042f2
cpreinholtz <cpr978@gmail.com>
2018-12-06 00:53:21 -0800

    Added files, updated Vidor to record telem

---
+++ vidor_command_and_status.py
@@ -0,0 +1,46 @@
+import socket
+import sys
+from thread import *
+
+HOST = ''   
+PORT = 8888
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+
+try:
+    s.bind((HOST, PORT))
+except socket.error , msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+
+print 'Socket bind complete'
+
+s.listen(10)
+print 'Socket now listening'
+
+def clientthread(conn):
+    #Sending message to connected client
+    conn.send('Welcome to the server. Receving Data...\n') 
+    f=open('out.txt','w')
+    #infinite loop so that function do not terminate and thread do not end.
+    while True:
+	
+        #Receiving from client
+        data = conn.recv(1024)
+        if not data:
+            break
+	f.write(data)
+    conn.close()
+
+#now keep talking with the client
+while 1:
+    #wait to accept a connection
+    conn, addr = s.accept()
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+
+    #start new thread
+    start_new_thread(clientthread ,(conn,))
+
+s.close()
+
